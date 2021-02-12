0af22a3333f58ffc47bd076df3d7d12f00dc318a
Surya <suryaprakash@tyfone.com>
2021-02-12 23:45:14 +0530

    Initial Commit.

---
+++ mobyralibrary/src/main/java/com/zitlab/mobyra/library/MobyraClient.java
@@ -0,0 +1,17 @@
+package com.zitlab.mobyra.library;
+
+/**
+ * The type Mobyra client.
+ */
+public class MobyraClient {
+
+    /**
+     * Instantiates a new Mobyra client.
+     *
+     * @param config the config
+     */
+    public MobyraClient(MobyraClient config){
+
+    }
+
+}
