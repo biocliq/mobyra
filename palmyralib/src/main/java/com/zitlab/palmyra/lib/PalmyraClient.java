d77cd09ffafef5c2b50f5d5768bd8f5cb196e880
Surya <surya@lachen.local>
2021-03-28 15:49:17 +0530

    Updated gradle with local maven repo

--- palmyralib/src/main/java/com/zitlab/palmyra/lib/PalmyraClient.java
+++ palmyralib/src/main/java/com/zitlab/palmyra/lib/PalmyraClient.java
@@ -1,14 +1,14 @@
 package com.zitlab.palmyra.lib;
 
-import com.zitlab.palmyra.http.MobyraClientBuilder;
+import com.zitlab.palmyra.ResponseCallback;
 import com.zitlab.palmyra.exception.PalmyraException;
+import com.zitlab.palmyra.http.MobyraClientBuilder;
 import com.zitlab.palmyra.http.PalmyraRestClient;
 
 /**
  * The type Mobyra client.
  */
 public final class PalmyraClient extends PalmyraRestClient {
-
 
     /**
      * Instantiates a new Mobyra client.
@@ -19,10 +19,8 @@ public final class PalmyraClient extends PalmyraRestClient {
         super(builder);
     }
 
-
     @Override
     protected <T> void sendCallback(ResponseCallback callback, boolean status, T response, PalmyraException ex) {
         callback.onMobyraResponse(status, response, ex);
     }
-
 }
